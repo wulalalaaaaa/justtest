98c3025fbe74e5c4466d58b17d7960f455762ee8
乌拉！ <92385785+wulalalaaaaa@users.noreply.github.com>
2023-07-03 17:09:32 +0800

    just a blank window

---
+++ framework.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+//以下已加入到pch.h
+
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN             // 从 Windows 头文件中排除极少使用的内容
+// Windows 头文件
+#include <windows.h>
+// C 运行时头文件
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+#include <iostream>
+#include <vector>
+#include <array>
+#include <bitset>
+#include <map>
